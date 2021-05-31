4b709b17bbc06c6f82743ec1ce196e2e7b8829cc
Suporno Chaudhury <suporno.chaudhury@gmail.com>
2021-05-31 16:08:15 +0530

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -10,14 +10,14 @@ GIT_SSH_COMMAND="ssh -v"
 echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 
-git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
-git remote set-url --push origin "$DESTINATION_REPO"
-git fetch -p origin
+#git clone --bare "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
+#git remote set-url --push origin "$DESTINATION_REPO"
+#git fetch -p origin
 # Exclude refs created by GitHub for pull request.
-git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
-git push --mirror
+#git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
+#git push --bare
 # Cleaning
-git checkout master
-git rm -r .github
-git commit -m "cleaning directory"
-git push origin master
+#git checkout master
+#git rm -r .github
+#git commit -m "cleaning directory"
+#git push origin master
