c33087789958b1122b826bd73c19129da30578d1
Suporno Chaudhury <suporno.chaudhury@gmail.com>
2021-05-31 15:46:26 +0530

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -11,9 +11,10 @@ echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 
 git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
-git rm -r .github/workflows
 git remote set-url --push origin "$DESTINATION_REPO"
 git fetch -p origin
+git checkout master
+git rm -r .github
 # Exclude refs created by GitHub for pull request.
 git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 git push --mirror
